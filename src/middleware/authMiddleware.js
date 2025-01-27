88a308a45da59f1d202c85207e75b6b319743653
opeyemi0001 <opeyemionanuga40@gmail.com>
2025-01-27 01:02:01 +0100

    feat: User Deletion

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -70,3 +70,15 @@ export const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
+
+// admin middleware
+export const adminMiddleware = asyncHandler(async (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    // if user is admin, move to the next middleware/controller
+    next();
+    return;
+  }
+  // if not admin send 403 Forbidden ---> terminate the request
+  res.status(403).json({message: "Only admins can do this!"});
+});
+
